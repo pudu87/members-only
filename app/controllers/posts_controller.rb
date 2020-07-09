0571740ec6473bb108506bc09c4a6e64bc2c42c6
pudu87 <sam.cappelle87@gmail.com>
2020-07-09 17:43:27 -0400

    create functional #index, #new, #create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+  include PostsHelper
+
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
